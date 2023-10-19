b8a7360ec6f17cdd3851d6f27a76b8f05b90ec0d
Claudiane Costa <103538940+claudianeCS@users.noreply.github.com>
2023-10-19 01:19:24 -0300

    update url

--- back/src/main/java/com/apisys/back/controller/auth/AuthenticationController.java
+++ back/src/main/java/com/apisys/back/controller/auth/AuthenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
-@RequestMapping("/auth/acess")
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     @Autowired
